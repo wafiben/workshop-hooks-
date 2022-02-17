728946ea1bb5f061ddffcade4a868725224c237a
wafiben <wafibenjeddou@gmail.com>
2022-02-17 20:45:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState ,useEffect} from "react";
+import Home from "./Home";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [show,setShow]=useState(false)
+const handleShow=()=>{
+  show ? setShow(false): setShow(true)
+}
+  const increment = () => {
+    setCount(count + 1);
+  };
+const decrement=()=>{
+  (count > 0) &&setCount(count-1)
+}
+//mounting
+/* useEffect(()=>{
+ alert('hello')
+}) */
+//updating
+/* useEffect(()=>{
+ alert('updating')
+},[count]) */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {" "}
+        <button onClick={increment}>+</button>
+      </h1>
+
+      <h1> {count}</h1>
+      <h1>
+        {" "}
+        <button onClick={decrement}>-</button>
+      </h1>
+      <button onClick={handleShow}>
+        Show
+      </button>
+      <div>
+        {show ?  <Home/>:null}
+     
+      </div>
     </div>
   );
 }
